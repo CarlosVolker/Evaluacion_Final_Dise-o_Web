fb43c9c861068398a90e962650d71a51ef4914db
CarlosVolker <143527813+CarlosVolker@users.noreply.github.com>
2024-05-18 18:34:20 -0400

    JS terminado|
    terminado html, JS
    solo falta corregir el CSS con el tamaño de las fotos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,26 @@ function showContent(sectionId, title) {
     const sections = document.querySelectorAll('main > section');
     const mainTitle = document.getElementById('main-title');
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var formulario = document.getElementById('formularioContacto');
+        formulario.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            var nombre = document.getElementById('nombre').value.trim();
+            var correo = document.getElementById('correo').value.trim();
+            var mensaje = document.getElementById('mensaje').value.trim();
+
+            if (nombre === "" || correo === "" || mensaje === ""){
+                alert('Por favor, complete todos los campos');
+            } else {
+                alert(`Formulario enviado:
+    Nombre  : ${nombre} 
+    Correo  : ${correo} 
+    Mensaje : ${mensaje}`);
+            }  
+        });
+    });
+
     sections.forEach(section => {
         if (section.id === sectionId) {
             section.style.display = 'block';
